9f40534a4a86d46fd2fd65191f6e9873857dc515
lcperri <prluisca@gmail.com>
2022-06-20 21:15:10 -0500

    prueba de commit styled components

---
+++ client/src/StyledComponents/Button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Button= styled.Button`
+    background: white;
+    cursor: pointer;
+    font-size: 14px;
+    margin: 4px;
+    padding: 4px 10px;
+    border: 2px solid #333;
+    border-radius: 30px;
+`
