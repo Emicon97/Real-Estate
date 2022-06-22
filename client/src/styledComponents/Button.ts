1f04ce69c4b298a1ed38463779f1dcdb6a687912
lcperri <prluisca@gmail.com>
2022-06-21 21:35:58 -0500

    estructura de componentes, react-router-dom y más styled components

--- client/src/styledComponents/Button.ts
+++ client/src/styledComponents/Button.ts
@@ -6,14 +6,13 @@ const Button = styled.button`
     font-size: 14px;
     margin: 4px;
     padding: 4px 10px;
-    border: 2px solid #333;
+    border: 2px solid #E52F50;
     border-radius: 30px;
     &:hover {
-        /* background-color: #333; */
-        /* color: #fff; */
+        background-color: #E52F50;
+        color: #fff;
         /* background: linear-gradient(45deg, #A3278F 0%, #E52F50 100%); */
-        border: 2px solid #E52F50;
+        /* border: 2px solid #E52F50; */
     }
 `
-
 export default Button
